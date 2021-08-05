6dcd89a2aa3921f3884512645be7a69861c6db4b
Eliezer Graber <eygraber@gmail.com>
2021-08-05 00:19:43 -0400

    Remove clock sample

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,8 @@
 rootProject.name = "cares"
 include(":core")
-include(":samples:simple-clock:desktop-app")
-findProject(":samples:simple-clock:desktop-app")?.name = "desktop-app"
-include("samples:simple-clock:app")
-findProject(":samples:simple-clock:app")?.name = "app"
-include("nav")
+include(":nav")
+
+include(":samples:nav-android")
 
 enableFeaturePreview("VERSION_CATALOGS")
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
